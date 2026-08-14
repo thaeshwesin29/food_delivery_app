f0cc5dec63692065f8be8095bee778ed4b176703
thaeshwesin29 <thaeshwesin2000@gmail.com>
2026-08-14 14:28:16 +0630

    feat:implement full auth, tabs navigation

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,15 +1,65 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
-import {router}  from "expo-router"
+import {View, Text, Button, Alert} from 'react-native'
+import {Link, router} from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
+import {useState} from "react";
+import {signIn} from "@/lib/appwrite";
+
 const SignIn = () => {
-  return (
-    <View>
-      <Text>sign-in</Text>
-      <Button title="Sign In" onPress={()=> router.push("/sign-up")}/>
-    </View>
-  );
-};
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [form, setForm] = useState({ email: '', password: '' });
 
-export default SignIn;
+    const submit = async () => {
+        const { email, password } = form;
 
-const styles = StyleSheet.create({});
+        if(!email || !password) return Alert.alert('Error', 'Please enter valid email address & password.');
+
+        setIsSubmitting(true)
+
+        try {
+            await signIn({ email, password });
+
+            router.replace('/');
+        } catch(error: any) {
+            Alert.alert('Error', error.message);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+
+    return (
+        <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+            <CustomInput
+                placeholder="Enter your email"
+                value={form.email}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+                label="Email"
+                keyboardType="email-address"
+            />
+            <CustomInput
+                placeholder="Enter your password"
+                value={form.password}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
+                label="Password"
+                secureTextEntry={true}
+            />
+
+            <CustomButton
+                title="Sign In"
+                isLoading={isSubmitting}
+                onPress={submit}
+            />
+
+            <View className="flex justify-center mt-5 flex-row gap-2">
+                <Text className="base-regular text-gray-100">
+                    Don't have an account?
+                </Text>
+                <Link href="/sign-up" className="base-bold text-primary">
+                    Sign Up
+                </Link>
+            </View>
+        </View>
+    )
+}
+
+export default SignIn
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,30 @@
 import {SplashScreen, Stack} from "expo-router";
 import { useFonts } from 'expo-font';
-import { useEffect } from "react";
-import "./global.css";
+import { useEffect} from "react";
 
-export default function RootLayout() {
+import './global.css';
+import * as Sentry from '@sentry/react-native';
+import useAuthStore from "@/store/auth.store";
+
+Sentry.init({
+  dsn: 'https://94edd17ee98a307f2d85d750574c454a@o4506876178464768.ingest.us.sentry.io/4509588544094208',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
   const [fontsLoaded, error] = useFonts({
     "Quicksand-Bold": require('../../assets/fonts/Quicksand-Bold.ttf'),
     "Quicksand-Medium": require('../../assets/fonts/Quicksand-Medium.ttf'),
@@ -13,9 +34,17 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if (error) throw error;
-    if (fontsLoaded) SplashScreen.hideAsync();
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
 
+  useEffect(() => {
+    fetchAuthenticatedUser()
+  }, []);
+
+  if(!fontsLoaded || isLoading) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
+
+Sentry.showFeedbackWidget();
